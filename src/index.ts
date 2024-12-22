c1fb18c2e90151329b76f8d1ef17ba83c5017829
Angga Afrianto <anggaaurelius@gmail.com>
2024-12-22 15:04:59 +0700

    validate with Yup

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import express from 'express'
-import router from './routes/api'
+import express from "express";
+import router from "./routes/api";
+import bodyParser from "body-parser";
 
-const app = express()
+const app = express();
 
-const PORT = 3000
+app.use(bodyParser.json());
+app.use("/api", router);
 
-app.use("/api", router)
+const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
