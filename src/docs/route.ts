cbf5430a42f855a575283f651a55513a43135faf
Angga Afrianto <anggaaurelius@gmail.com>
2025-03-17 15:45:14 +0700

    fix: swagger blank page

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -1,6 +1,8 @@
 import { Express } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "./swagger_output.json";
+const swaggerUICss =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
 
 export default function docs(app: Express) {
   app.use(
@@ -9,8 +11,7 @@ export default function docs(app: Express) {
     swaggerUi.setup(swaggerOutput, {
       customCss:
         ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }",
-      customCssUrl:
-        "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css",
+      customCssUrl: swaggerUICss,
     })
   );
 }
